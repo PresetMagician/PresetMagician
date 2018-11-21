1b673c84784b570cc100f88857c9b3e4e4c8e9e7
Felicia Hummel <felicia@drachenkatze.org>
2018-11-21 07:40:46 +0100

    Added plugin info window

---
+++ PresetMagician/GUI/PluginInfoWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Drachenkatze.PresetMagician.VSTHost.VST;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Forms.VisualStyles;
+
+namespace Drachenkatze.PresetMagician.GUI.GUI
+{
+    /// <summary>
+    /// Interaction logic for PluginInfoWindow.xaml
+    /// </summary>
+    public partial class PluginInfoWindow : Window
+    {
+        public PluginInfoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
