b304bcd8ff488da578101697229af3b072b84035
Felicia Hummel <felicia@drachenkatze.org>
2018-11-21 07:41:09 +0100

    Added preset parser

---
+++ Drachenkatze.PresetMagician.VendorPresetParser/D16 Group/PunchBox/D16PunchBox.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drachenkatze.PresetMagician.VendorPresetParser.D16_Group.PunchBox
+{
+    public class D16PunchBox : IVendorPresetParser
+    {
+        public List<int> SupportedPlugins => new List<int> { 1347306072 };
+    }
+}
