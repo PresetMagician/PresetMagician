36a7a4446443a53235837263f58a0f2cff12acab
Felicia Hummel <felicia@drachenkatze.org>
2018-11-21 07:41:09 +0100

    Added preset parser

---
+++ Drachenkatze.PresetMagician.VendorPresetParser/IVendorPresetParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drachenkatze.PresetMagician.VendorPresetParser
+{
+    internal interface IVendorPresetParser
+    {
+        List<int> SupportedPlugins { get; }
+    }
+}
