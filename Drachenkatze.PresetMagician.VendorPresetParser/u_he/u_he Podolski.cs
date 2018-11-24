0d47430f58bebad6d9ba4ff6252d47f62007b120
Felicia Hummel <felicia@drachenkatze.org>
2018-11-24 08:04:25 +0100

    Added support for many u_he plugins

---
+++ Drachenkatze.PresetMagician.VendorPresetParser/u_he/u_he Podolski.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Drachenkatze.PresetMagician.VendorPresetParser.u_he
+{
+    internal class u_he_Podolski : u_he, IVendorPresetParser
+    {
+        public override List<int> SupportedPlugins => new List<int> { 1349477487 };
+
+        public void ScanBanks()
+        {
+            H2PScanBanks("Podolski.data", "Podolski", false);
+        }
+    }
+}
---
+++ Drachenkatze.PresetMagician.VendorPresetParser/u_he/u_he.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Shell32;
+using System.IO;
+using System.Threading;
+using Drachenkatze.PresetMagician.VSTHost.VST;
+using File = System.IO.File;
+
+namespace Drachenkatze.PresetMagician.VendorPresetParser.u_he
+{
+    public abstract class u_he : AbstractVendorPresetParser
+    {
+        public void H2PScanBanks(string dataDirectoryName, string productName, bool userPresets)
+        {
+            if (Banks == null)
+            {
+                Banks = new List<PresetBank>();
+            }
+
+            var presetDirectory = GetPresetDirectory(dataDirectoryName, productName, userPresets);
+            var directoryInfo = new DirectoryInfo(presetDirectory);
+
+            string bankName = "Factory Bank";
+            if (userPresets)
+            {
+                bankName = "User Bank";
+            }
+            Banks.Add(H2PScanBank(bankName, directoryInfo));
+            foreach (var directory in directoryInfo.EnumerateDirectories())
+            {
+                Banks.Add(H2PScanBank(directory.Name, directory));
+            }
+        }
+
+        public PresetBank H2PScanBank(string name, DirectoryInfo directory)
+        {
+            PresetBank bank = new PresetBank
+            {
+                BankName = name
+            };
+
+            foreach (var file in directory.EnumerateFiles())
+            {
+                VSTPreset vstPreset = new VSTPreset();
+                vstPreset.PresetName = file.Name.Replace(".h2p", "");
+                vstPreset.SetPlugin(VstPlugin);
+                vstPreset.BankName = bank.BankName;
+
+                var fs = file.OpenRead();
+
+                vstPreset.PresetData = new byte[fs.Length];
+                fs.Read(vstPreset.PresetData, 0, (int)fs.Length);
+                fs.Close();
+
+                bank.VSTPresets.Add(vstPreset);
+            }
+
+            return bank;
+        }
+
+        public string getDataDirectory(string dataDirectoryName)
+        {
+            var vstPluginsPath = Path.GetDirectoryName(VstPlugin.PluginDLLPath);
+
+            return Path.Combine(vstPluginsPath, dataDirectoryName);
+        }
+
+        public string GetPresetDirectory(string dataDirectoryName, string productName, bool userPresets)
+        {
+            object[] args = new object[] { getDataDirectory(dataDirectoryName), productName, userPresets };
+
+            Thread staThread = new Thread(new ParameterizedThreadStart(GetPresetDirectorySTA));
+            staThread.SetApartmentState(ApartmentState.STA);
+            staThread.Start(args);
+            staThread.Join();
+
+            Debug.WriteLine(args[0]);
+            return (string)args[0];
+        }
+
+        public void GetPresetDirectorySTA(object param)
+        {
+            object[] args = (object[])param;
+            string dataDirectoryName = (string)args[0];
+            string productName = (string)args[1];
+            bool userPresets = (bool)args[2];
+
+            var shortCutDataDirectoryName = getDataDirectory(dataDirectoryName + ".lnk");
+
+            Debug.WriteLine("Estimated shortcut directory name is " + shortCutDataDirectoryName);
+            string dataDirectory;
+
+            if (IsShortcut(shortCutDataDirectoryName))
+            {
+                dataDirectory = ResolveShortcut(getDataDirectory(shortCutDataDirectoryName));
+            }
+            else
+            {
+                dataDirectory = getDataDirectory(dataDirectoryName);
+            }
+
+            Debug.WriteLine("Data directory is " + dataDirectory);
+
+            if (userPresets)
+            {
+                args[0] = Path.Combine(dataDirectory, "UserPresets", productName);
+            }
+            else
+            {
+                args[0] = Path.Combine(dataDirectory, "Presets", productName);
+            }
+        }
+
+        public static bool IsShortcut(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return false;
+            }
+
+            var directory = Path.GetDirectoryName(path);
+            var file = Path.GetFileName(path);
+
+            var shell = new Shell();
+            var folder = shell.NameSpace(directory);
+            var folderItem = folder.ParseName(file);
+
+            return folderItem.IsLink;
+        }
+
+        public static string ResolveShortcut(string path)
+        {
+            var directory = Path.GetDirectoryName(path);
+            var file = Path.GetFileName(path);
+            if (!IsShortcut(path))
+            {
+                return string.Empty;
+            }
+
+            var shell = new Shell();
+            var folder = shell.NameSpace(directory);
+            var folderItem = folder.ParseName(file);
+            if (folderItem == null)
+            {
+                return string.Empty;
+            }
+
+            var link = (ShellLinkObject)folderItem.GetLink;
+            return link.Target.Path;
+        }
+    }
+}
