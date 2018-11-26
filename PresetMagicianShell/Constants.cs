ea68fca40fa5a73d11acc27b40bbb30ff4c5dd60
Felicia Hummel <felicia@drachenkatze.org>
2018-11-26 06:34:21 +0100

    Foo

---
+++ PresetMagicianShell/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresetMagicianShell
+{
+    public static class Commands
+    {
+        public static class Tools
+        {
+            public const string NKSFViewer = "Tools.NKSFViewer";
+        }
+    }
+}
