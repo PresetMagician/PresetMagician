4b7b38801c5f8fd6c8a598b2eed116727d426440
Felicia Hummel <felicia@drachenkatze.org>
2018-11-28 15:53:25 +0100

    Added english as the default (and only supported) culture right now

--- PresetMagicianShell/App.xaml.cs
+++ PresetMagicianShell/App.xaml.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using Catel.IoC;
 using Catel.Logging;
+using Catel.Services;
 using Orchestra.Services;
 using Orchestra.Views;
 using PresetMagicianShell.Views;
@@ -31,8 +33,11 @@ namespace PresetMagicianShell
 #if DEBUG
             LogManager.AddDebugListener(false);
 #endif
-
             var serviceLocator = ServiceLocator.Default;
+
+            var languageService = ServiceLocator.Default.ResolveType<ILanguageService>();
+            languageService.PreferredCulture = new CultureInfo("en-US");
+            languageService.FallbackCulture = new CultureInfo("en-US");
 
             var shellService = serviceLocator.ResolveType<IShellService>();
 
