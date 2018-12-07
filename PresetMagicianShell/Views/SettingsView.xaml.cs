f4a50b5f7e1ba6f4e02f41fc20dd7ae2ce8e242f
Felicia Hummel <felicia@drachenkatze.org>
2018-12-07 20:44:52 +0100

    Initial settings implementation

---
+++ PresetMagicianShell/Views/SettingsView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace PresetMagicianShell.Views
+{
+    public partial class SettingsView 
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
