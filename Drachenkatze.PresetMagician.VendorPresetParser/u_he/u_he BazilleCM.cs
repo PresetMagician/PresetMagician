6f6c410899e8f2b1643013dfe71310dab8e6b604
Felicia Hummel <felicia@drachenkatze.org>
2018-12-26 02:32:24 +0100

    Added ZebraCM and BazilleCM

---
+++ Drachenkatze.PresetMagician.VendorPresetParser/u_he/u_he BazilleCM.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections.Generic;
+
+namespace Drachenkatze.PresetMagician.VendorPresetParser.u_he
+{
+    internal class u_he_BazilleCM : u_he, IVendorPresetParser
+    {
+        public override List<int> SupportedPlugins => new List<int> { 1967276877 };
+
+        public void ScanBanks()
+        {
+            H2PScanBanks("BazilleCM.data", "BazilleCM", false);
+        }
+    }
+}
