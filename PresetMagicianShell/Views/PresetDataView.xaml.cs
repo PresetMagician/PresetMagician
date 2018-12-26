ec7554d0040b23002b43c0a79a7ab6cce4280aec
Felicia Hummel <felicia@drachenkatze.org>
2018-12-26 10:21:10 +0100

    Support for quite a few plugins

---
+++ PresetMagicianShell/Views/PresetDataView.xaml.cs
@@ -0,0 +1,26 @@
+using System.Windows.Controls;
+using Be.Windows.Forms;
+using Catel.Windows;
+using PresetMagicianShell.ViewModels;
+
+namespace PresetMagicianShell.Views
+{
+    public partial class PresetDataView 
+    {
+        public PresetDataView()
+        {
+            InitializeComponent();
+        }
+        
+        public PresetDataView(PresetDataViewModel viewModel)
+            : base(viewModel, DataWindowMode.Close)
+        {
+            AddCustomButton(new DataWindowButton("Open with HxD", "OpenWithHxD"));
+
+            InitializeComponent();
+
+            var provider = new DynamicByteProvider(viewModel.Preset.PresetData);
+            PresetDataControl.ByteProvider = provider;
+        }
+    }
+}
