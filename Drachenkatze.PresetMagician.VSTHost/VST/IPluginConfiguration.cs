9eb7816327b58f0c09a3025ed1a9925649ff4ace
Drachenkatze <felicia@drachenkatze.org>
2019-01-02 09:44:56 +0100

    Addd plugin config, added initial audio preview delay

---
+++ Drachenkatze.PresetMagician.VSTHost/VST/IPluginConfiguration.cs
@@ -0,0 +1,11 @@
+using Drachenkatze.PresetMagician.NKSF.NKSF;
+
+namespace PresetMagician.Models
+{
+    public interface IPluginConfiguration
+    {
+        bool IsEnabled { get; set; }
+        int AudioPreviewPreDelay { get; set; }
+        ControllerAssignments DefaultControllerAssignments { get; set; }
+    }
+}
