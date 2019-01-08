1f8d3db3db3d3c650b8477e05bfaa212b8b68a04
Drachenkatze <felicia@drachenkatze.org>
2019-01-08 23:20:16 +0100

    Added synister

---
+++ Drachenkatze.PresetMagician.VendorPresetParser/TUBerln/Synister.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Drachenkatze.PresetMagician.VendorPresetParser.Common;
+
+namespace Drachenkatze.PresetMagician.VendorPresetParser.TUBerln
+{
+    public class Synister: AbstractVendorPresetParser, IVendorPresetParser
+    {
+        public override List<int> SupportedPlugins => new List<int> {1298748272};
+
+
+        public void ScanBanks()
+        {
+            var directory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments),
+                @"Synister");
+
+            var vc2parser = new VC2Parser(VstPlugin, "xml",Presets);
+            vc2parser.DoScan(RootBank, directory);
+        }
+    }
+}
