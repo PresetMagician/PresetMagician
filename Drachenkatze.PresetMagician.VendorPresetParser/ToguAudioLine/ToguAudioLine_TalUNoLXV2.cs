473d749b0b849a9dcce511cc9bdfcef68ff1735d
Drachenkatze <felicia@drachenkatze.org>
2019-01-09 20:16:28 +0100

    Added TAL plugins

---
+++ Drachenkatze.PresetMagician.VendorPresetParser/ToguAudioLine/ToguAudioLine_TalUNoLXV2.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Drachenkatze.PresetMagician.VendorPresetParser.Common;
+using JetBrains.Annotations;
+
+namespace Drachenkatze.PresetMagician.VendorPresetParser.ToguAudioLine
+{
+    // ReSharper disable once InconsistentNaming
+    [UsedImplicitly]
+    public class ToguAudioLine_TalUNoLXV2 : ToguAudioLine, IVendorPresetParser
+    {
+        public override List<int> SupportedPlugins => new List<int> {1970171700};
+
+        private static readonly string ParseDirectory = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            @"ToguAudioLine\TAL-U-No-LX\presets");
+        
+        public override string BankLoadingNotes { get; set; } = $"Presets are loaded from {ParseDirectory}. First sub-folder defines the bank.";
+
+        public void ScanBanks()
+        {
+            DoScan(RootBank, ParseDirectory);
+        }
+        
+        protected void DoScan(PresetBank rootBank, string directory)
+        {
+            var vc2parser = new VC2Parser(VstPlugin, "pjunoxl",Presets);
+            vc2parser.DoScan(rootBank, directory);
+        }
+    }
+  
+}
