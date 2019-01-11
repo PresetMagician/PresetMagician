1fab4300e1bb256ae4389e5842f8c91749a40a43
Drachenkatze <felicia@drachenkatze.org>
2019-01-11 17:18:45 +0100

    Added Thorn and ThornCM

--- Drachenkatze.PresetMagician.VendorPresetParser/DmitrySches/DmitrySches_ThornCM.cs
+++ Drachenkatze.PresetMagician.VendorPresetParser/DmitrySches/DmitrySches_ThornCM.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using Drachenkatze.PresetMagician.VendorPresetParser.Common;
 using JetBrains.Annotations;
 
 namespace Drachenkatze.PresetMagician.VendorPresetParser.DmitrySches
