2698dd3c38ef4efdd29b702aa4726ee5a37016c7
Drachenkatze <felicia@drachenkatze.org>
2019-02-24 10:30:28 +0100

    added perf test

--- PresetMagician.Tests/ModelTests/ApplicationTests.cs
+++ PresetMagician.Tests/ModelTests/ApplicationTests.cs
@@ -2,9 +2,12 @@ using System.Collections.ObjectModel;
 using System.Data.Entity;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Sockets;
 using Catel.Data;
+using FluentAssertions;
 using SharedModels;
 using SharedModels.Collections;
+using TrackableEntities.Common;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -14,37 +17,52 @@ namespace PresetMagician.Tests.ModelTests
     public class ApplicationTests
     {
         private readonly ITestOutputHelper output;
-        private readonly DatabaseFixture _databaseFixture; 
-        
+        private readonly DatabaseFixture _databaseFixture;
+
         public ApplicationTests(ITestOutputHelper output, DatabaseFixture databaseFixture)
         {
             this.output = output;
             _databaseFixture = databaseFixture;
         }
-        
+
         [Fact]
         public void TestFoo()
         {
             var manager = _databaseFixture.GetTestDataManager();
 
-            ObservableCollection<Plugin> plugins; 
-            
             using (var dbContext = manager.Create())
             {
                 dbContext.Migrate();
-                ModelBase.DisablePropertyChangeNotifications = true;
-                dbContext.Database.Log = delegate(string s) { output.WriteLine(s);};
-                dbContext.Plugins.Include(plugin => plugin.AdditionalBankFiles).Include(plugin => plugin.PluginLocation).Load();
+                dbContext.Database.Log = delegate(string s) { output.WriteLine(s); };
+                
                 Stopwatch stopWatch = new Stopwatch();
                 stopWatch.Start();
-                dbContext.Plugins.Include(plugin => plugin.AdditionalBankFiles).Include(plugin => plugin.PluginLocation).Include(plugin => plugin.Presets).Load();
-                plugins = dbContext.Plugins.Local;
+                var modesList = dbContext.Modes.ToList();
+
+                stopWatch.ElapsedMilliseconds.Should().BeLessThan(1000);
                 
+                stopWatch.Restart();
+                var typesList = dbContext.Types.ToList();
+                stopWatch.ElapsedMilliseconds.Should().BeLessThan(1000);
+                
+                stopWatch.Restart();
+                var pluginList = dbContext.Plugins.Include(p => p.DefaultModes).Include(p => p.DefaultTypes).ToList();
+                stopWatch.ElapsedMilliseconds.Should().BeLessThan(3000);
+               
+                stopWatch.Restart();
+                var presetsList = dbContext.Presets.Include(p => p.Modes).Include(p => p.Types).ToList();
+                stopWatch.ElapsedMilliseconds.Should().BeLessThan(6000);
+               
+                
+               
+               
+
+
+                var hive = (from preset in pluginList where preset.Id == 29 select preset).FirstOrDefault();
+                output.WriteLine(hive.Presets.Count.ToString());
                 stopWatch.Stop();
                 output.WriteLine(stopWatch.Elapsed.ToString());
             }
-            
-            output.WriteLine(plugins.Count.ToString());
         }
     }
 }
--- PresetMagicianScratchPad/Program.cs
+++ PresetMagicianScratchPad/Program.cs
@@ -1,67 +1,110 @@
 ﻿using System;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
+using System.Data.SQLite;
 using System.Diagnostics;
-using System.IO;
-using System.Threading;
-using Catel.Collections;
-using Catel.Data;
-using Drachenkatze.PresetMagician.Utils;
-using SharedModels;
 using System.Linq;
-using Catel.IoC;
-using Catel.Runtime.Serialization;
-using MethodTimer;
-using PresetMagician.Serialization;
-using Catel.Runtime.Serialization.Xml;
-using XmlSerializer = System.Xml.Serialization.XmlSerializer;
+using SQLite.CodeFirst;
 
 namespace PresetMagicianScratchPad
 {
+    public class Mode
+    {
+        [Key] public int Id { get; set; }
+
+        public ICollection<Plugin> Plugins { get; set; }
+        public ICollection<Preset> Presets { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class Type
+    {
+        [Key] public int Id { get; set; }
+
+        public ICollection<Plugin> Plugins { get; set; }
+        public ICollection<Preset> Presets { get; set; }
+
+        public string Name { get; set; }
+        public string SubTypeName { get; set; }
+    }
+
+    public class Plugin
+    {
+        [Key] public int Id { get; set; }
+        public ICollection<Preset> Presets { get; set; }
+
+        public ICollection<Type> DefaultTypes { get; set; }
+        public ICollection<Mode> DefaultModes { get; set; }
+    }
+
+    public class Preset
+    {
+        [Key] public int Id { get; set; }
+        public Plugin Plugin { get; set; }
+        public ICollection<Type> Types { get; set; }
+        public ICollection<Mode> Modes { get; set; }
+    }
+
+    public class ApplicationDatabaseContext : DbContext
+    {
+        public ApplicationDatabaseContext() : base(new SQLiteConnection(GetConnectionString()), true)
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ValidateOnSaveEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        public static string GetConnectionString()
+        {
+            var cs = new SQLiteConnectionStringBuilder()
+            {
+                DataSource = "foo.sqlite3", ForeignKeys = false, SyncMode = SynchronizationModes.Off,
+                CacheSize = -10240
+            };
+
+
+            return cs.ConnectionString;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var sqliteConnectionInitializer =
+                new SqliteCreateDatabaseIfNotExists<ApplicationDatabaseContext>(modelBuilder);
+            modelBuilder.Entity<Plugin>().HasMany(p => p.DefaultModes).WithMany(q => q.Plugins).Map(mc =>
+                mc.MapLeftKey("PluginId").MapRightKey("ModeId").ToTable("PluginModes"));
+
+            modelBuilder.Entity<Plugin>().HasMany(p => p.DefaultTypes).WithMany(q => q.Plugins).Map(mc =>
+                mc.MapLeftKey("PluginId").MapRightKey("TypeId").ToTable("PluginTypes"));
+
+
+            modelBuilder.Entity<Preset>().HasMany(p => p.Types).WithMany(q => q.Presets).Map(mc =>
+                mc.MapLeftKey("PresetId").MapRightKey("TypeId").ToTable("PresetTypes"));
+
+            modelBuilder.Entity<Preset>().HasMany(p => p.Modes).WithMany(q => q.Presets).Map(mc =>
+                mc.MapLeftKey("PresetId").MapRightKey("ModeId").ToTable("PresetModes"));
+            Database.SetInitializer(sqliteConnectionInitializer);
+        }
+
+        public DbSet<Plugin> Plugins { get; set; }
+        
+    }
+
+
     public class Program
     {
         [STAThread]
         static void Main(string[] args)
         {
-            ServiceLocator.Default.RegisterType<IXmlSerializer, TestSerializer>();
-            ServiceLocator.Default.RegisterType<ISerializer, TestSerializer>();
+            using (var context = new ApplicationDatabaseContext())
+            {
+                context.Plugins
+                    .Include("Presets.Modes")
+                    .Include("Presets.Types")
+                    .AsNoTracking().ToList();
+            }
             
-            var plugin = new Plugin();
-            var preset = new Preset {Bar = "I'm unmodified, too!"};
-            plugin.Presets.Add(preset);
-
-            var ms = new MemoryStream();
-            XmlSerializer xs = new XmlSerializer(typeof(Plugin));
-            xs.Serialize(ms, plugin);
-            
-            
-            (plugin as IEditableObject).BeginEdit();
-            preset.Foo = "I'm now modified";
-            preset.Bar = "I'm now modified as well";
-            (plugin as IEditableObject).CancelEdit();
-
-            Debug.WriteLine(preset.Foo);
-            Debug.WriteLine(preset.Bar);
-
-     
-        }
-
-    }
-    
-    public class Preset: ModelBase
-    {
-        public string Foo { get; set; } = "I'm unmodified";
-        public string Bar { get; set; }
-    
-    }
-
-    public class Plugin : ChildAwareModelBase 
-    {
-        [IncludeInSerialization]
-        public ObservableCollection<Preset> Presets { get; set; } = new ObservableCollection<Preset>();
-        public void ClearDirty () {
-            IsDirty = false;
         }
     }
 }
