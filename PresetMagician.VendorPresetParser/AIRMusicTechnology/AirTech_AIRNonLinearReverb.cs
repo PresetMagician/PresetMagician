d656e5ffb03fd4382fac443993422f584993ff67
Drachenkatze <felicia@drachenkatze.org>
2019-03-31 15:32:18 +0200

    Finalized air tech stuff, parse serum cfg

---
+++ PresetMagician.VendorPresetParser/AIRMusicTechnology/AirTech_AIRNonLinearReverb.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using JetBrains.Annotations;
+using PresetMagician.Core.Interfaces;
+using PresetMagician.VendorPresetParser.AIRMusicTechnology.Tfx;
+
+namespace PresetMagician.VendorPresetParser.AIRMusicTechnology
+{
+    // ReSharper disable once InconsistentNaming
+    [UsedImplicitly]
+    public class AirTech_AIRNonLinearReverb : AirTech, IVendorPresetParser
+    {
+        public override List<int> SupportedPlugins => new List<int> {1380732481};
+        protected override string Extension { get; } = "tfx";
+
+        public override string Remarks { get; set; } =
+            "Audio Previews are non-functional for this plugin";
+
+        protected override string GetParseDirectory()
+        {
+            return Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86),
+                @"AIR Music Technology\AIR Non-Linear Reverb\Presets");
+        }
+
+        protected override Tfx.Tfx GetTfxParser()
+        {
+            return new TfxAIRNonLinearReverb();
+        }
+    }
+}
---
+++ PresetMagician.VendorPresetParser/AIRMusicTechnology/Tfx/TfxAIRChorus.cs
@@ -0,0 +1,26 @@
+using PresetMagician.VendorPresetParser.Properties;
+
+namespace PresetMagician.VendorPresetParser.AIRMusicTechnology.Tfx
+{
+    public class TfxAIRChorus : Tfx
+    {
+        public override byte[] BlockMagic { get; } = {0x2f,0x44,0x65,0x66,};
+        public override bool IncludeMidi { get; } = true;
+        public override bool IncludePatchNameAtEnd { get; } = true;
+
+       public override string GetMagicBlockPluginName()
+       {
+           return "";
+       }
+        
+        public override byte[] GetMidiData()
+        {
+            return new byte[0];
+        }
+        
+        public override byte[] GetEndChunk()
+        {
+            return VendorResources.AirFxSuiteEndChunk;
+        }
+    }
+}
