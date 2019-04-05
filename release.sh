ef5a45c7ffa4703e13dc48ac1f4736e3522ea041
Drachenkatze <felicia@drachenkatze.org>
2019-04-06 00:57:58 +0200

    Updated release script

--- release.sh
+++ release.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+VERSION=`cat CommonAssemblyInfo.cs|grep AssemblyVersion | sed -nr 's/.*"(.*)".*/\1/p'`
 cd Releases
+cp Setup.exe Setup-$VERSION.exe
+
 rsync -av * presetmagician.com@miau.mgmt.drachenkatze.org:/home/presetmagician.com/PresetMagicianSite/public/downloads/alpha/
 cd ..
-echo "CHANGEVERSION" >> PresetMagician/Properties/AssemblyInfo.cs
+echo "CHANGEVERSION" >> CommonAssemblyInfo.cs
