d02335e3191ca66ae20805f669ac6395ec6cff26
Drachenkatze <felicia@drachenkatze.org>
2019-06-19 16:42:23 +0200

    Use unsigned ints for NKS

--- PresetMagician.NKS/PluginId.cs
+++ PresetMagician.NKS/PluginId.cs
@@ -5,6 +5,6 @@ namespace PresetMagician.NKS
     [MessagePackObject]
     public class PluginId
     {
-        [Key("VST.magic")] public int VSTMagic;
+        [Key("VST.magic")] public uint VSTMagic;
     }
 }
