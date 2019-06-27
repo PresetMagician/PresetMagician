44c88c9d3f517c386611942bd5df31c4e90f437e
Drachenkatze <felicia@drachenkatze.org>
2019-06-27 19:45:10 +0200

    0.9.0 release

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.6.4")]
-[assembly: AssemblyFileVersion("0.6.4")]
-[assembly: AssemblyInformationalVersion("0.6.4")]
+[assembly: AssemblyVersion("0.9.0")]
+[assembly: AssemblyFileVersion("0.9.0")]
+[assembly: AssemblyInformationalVersion("0.9.0")]
---
+++ release-alpha.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git checkout alpha
+VERSION=`cat CommonAssemblyInfo.cs|grep AssemblyVersion | sed -nr 's/.*"(.*)".*/\1/p'`
+cd Releases
+cp Setup.exe PresetMagicianSetup-$VERSION.exe
+rm PresetMagicianSetup.exe
+mv Setup.exe PresetMagicianSetup.exe
+
+rsync -av * presetmagician.com@miau.mgmt.drachenkatze.org:/home/presetmagician.com/PresetMagicianSite/public/downloads/alpha/
+cd ..
+echo "CHANGEVERSION" >> CommonAssemblyInfo.cs
