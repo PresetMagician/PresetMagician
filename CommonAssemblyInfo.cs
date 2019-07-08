dbcab7e99d77093ddb902a401e93923c4af7f519
Drachenkatze <felicia@drachenkatze.org>
2019-07-08 18:47:36 +0200

    Push to 0.9.1

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.9.0")]
-[assembly: AssemblyFileVersion("0.9.0")]
-[assembly: AssemblyInformationalVersion("0.9.0")]
+[assembly: AssemblyVersion("0.9.1")]
+[assembly: AssemblyFileVersion("0.9.1")]
+[assembly: AssemblyInformationalVersion("0.9.1")]
